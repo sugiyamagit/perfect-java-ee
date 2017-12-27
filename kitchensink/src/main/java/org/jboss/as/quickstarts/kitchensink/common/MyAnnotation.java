024c35c326d2b720ed747059a8f744763bf6b597
Sakuya Inokuma <rip.sk0207@gmail.com>
2017-12-28 02:32:54 +0900

    add reference app(kitchensink)

---
+++ kitchensink/src/main/java/org/jboss/as/quickstarts/kitchensink/common/MyAnnotation.java
@@ -0,0 +1,33 @@
+/**
+ *
+ */
+package org.jboss.as.quickstarts.kitchensink.common;
+
+import static java.lang.annotation.ElementType.*;
+import static java.lang.annotation.RetentionPolicy.*;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import javax.enterprise.util.Nonbinding;
+import javax.inject.Qualifier;
+
+/**
+ * 限定子サンプル
+ *
+ * @author sinokuma
+ *
+ */
+@Qualifier
+@Retention(RUNTIME)
+@Target({METHOD, FIELD, PARAMETER, TYPE})
+public @interface MyAnnotation {
+    /** 値を差替可能にする */
+    MyAnnotationStrategy value();
+
+    /** 依存性解決に使用されない。コメント欄をアノテーションに作ることができる */
+    @Nonbinding
+    String comment();
+
+    // 組込み限定子:@Any, @Default, @New(@NewはCDI1.1より非推奨)
+}
---
+++ kitchensink/src/main/java/org/jboss/as/quickstarts/kitchensink/common/MyAnnotationStrategy.java
@@ -0,0 +1,12 @@
+/**
+ *
+ */
+package org.jboss.as.quickstarts.kitchensink.common;
+
+/**
+ * @author sinokuma
+ *
+ */
+public enum MyAnnotationStrategy {
+    VALUE_A, VALUE_B
+}
